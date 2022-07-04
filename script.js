255448e95db1cec63332d9b811e04f20de7048f6
Andrew <andrewoaksoekhant@gmail.com>
2022-07-04 22:06:39 +0800

    added math logic , fixed backspace and dot buttons

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 let num1 = 0;
 let num2 = 0;
+let num3 = 0;
+let opr = "";
+let calc = false;
+let clear = false;
+let enable = true;
 
 //queries
-let nums = document.querySelectorAll(".row button");
+let nums = document.querySelectorAll('.row button');
+let btns = document.querySelectorAll('.btn');
+
 let win = document.querySelector(".window");
 let ac = document.querySelector('.ac');
 let neg = document.querySelector('.neg');
 let back = document.querySelector('.back');
-
-// arithmetic functions
-let add = (a, b) => a + b;
-let sub = (a, b) => a - b;
-let mul = (a, b) => a * b;
-let div = (a, b) => a / b;
+let eq = document.querySelector('.equals');
 
 //clear display
 ac.addEventListener('click', () => {
     num1 = 0;
     num2 = 0;
+    num3 = 0;
+    calc = false;
+    clear = false;
     win.textContent= "";
 })
 
@@ -31,22 +36,81 @@ neg.addEventListener('click', () => {
     }
 });
 
-//backspace button
+// backspace button
 back.addEventListener('click', () => {
-    win.textContent = win.textContent.slice(0,win.textContent.length-1);
+    if (enable)
+        win.textContent = win.textContent.slice(0,win.textContent.length-1);
 })
 
+// math buttons
+btns.forEach((e) => {
+    e.addEventListener('click', mathBtn);
+});
+
+// equals button
+eq.addEventListener('click', () => {
+    clear = true;
+    if (calc) {
+        num2 = win.textContent;
+        num3 = mathLogic(opr);
+        win.textContent = num3;
+        calc = false;
+    }
+})
 
 //updates display when button pressed
 nums.forEach((e) => {
-    if (!isNaN(e.textContent) || e.textContent == ".") {
+    if (!isNaN(e.textContent)) {
         e.addEventListener('click', addWindow);
     } 
+    if (e.textContent == ".") {
+        e.addEventListener('click', () => {
+            if (!win.textContent.includes('.'))
+            win.textContent += e.textContent;
+        })
+    }
 });
 
 function addWindow() {
+    if (clear) {
+        win.textContent = "";
+        clear = false;
+        enable = true;
+    }
     win.textContent += this.textContent;
 }
 
+function mathBtn() {
+    clear = true;
+    enable = false;
+    if (calc) {
+        num2 = win.textContent;
+        num3 = mathLogic(opr);
+        win.textContent = num3;
+        num1 = num3;
+    } else {
+        num1 = win.textContent;
+        calc = true;
+    }
+    opr = this.textContent;
+}
 
+function mathLogic(symbol) {
+    let result = 0;
+    switch (symbol) {
+        case '+':
+            result = parseInt(num1) + parseInt(num2);
+            break;
+        case '-':
+            result = parseInt(num1) - parseInt(num2);
+            break;
+        case '*':
+            result = parseInt(num1) * parseInt(num2);
+            break;
+        case '/':
+            result = parseInt(num1) / parseInt(num2);   
+            break;
+    }
+    return result;
+}
 
